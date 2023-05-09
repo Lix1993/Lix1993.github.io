651c464163a78e082f337d05a811cf92702e92c7
Li Xing <lix930701@gmail.com>
2023-05-09 15:40:14 +0800

    启用PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,10 @@ module.exports = {
   description: "简历 for Li Xing",
   head: [
     ["meta", { name: "keywords", content: '简历' }],
-    // ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "icon", href: `/icons/icon-521x521.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   base: "/",
   dest: "./dist",
@@ -21,7 +24,15 @@ module.exports = {
     },
     lastUpdated: 'Last Updated'
   },
-
+  plugins: [
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: {
+        message: "New content is available.",
+        buttonText: "Refresh"
+      }
+    }]
+  ],
   markdown: {
     // options for markdown-it-anchor
     anchor: { permalink: false },
