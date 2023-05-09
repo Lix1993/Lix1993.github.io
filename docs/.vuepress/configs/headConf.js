5c90a870db3f7c493565bdf63a436d68686dc163
Li Xing <lix930701@gmail.com>
2023-05-09 16:46:49 +0800

    typofix

--- docs/.vuepress/configs/headConf.js
+++ docs/.vuepress/configs/headConf.js
@@ -1,6 +1,6 @@
 module.exports = [
     ["meta", { name: "keywords", content: '简历' }],
-    ["link", { rel: "icon", href: `/icons/icon-521x521.png` }],
+    ["link", { rel: "icon", href: `/icons/icon-512x512.png` }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
