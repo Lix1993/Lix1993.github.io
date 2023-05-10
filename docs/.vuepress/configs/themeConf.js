49afa15c16fba2044a1e8fc38edb6561335d2acc
Li Xing <lix930701@gmail.com>
2023-05-10 11:23:00 +0800

    修改导航栏

--- docs/.vuepress/configs/themeConf.js
+++ docs/.vuepress/configs/themeConf.js
@@ -1,10 +1,9 @@
 module.exports = {
     search: false,
     nav: [
-        { text: "Home", link: "/" },
+        { text: "Resume", link: "/" },
         { text: "Projects", link: "/projects/" },
         { text: "Skills", link: "/skills/" },
-        { text: "GitHub", link: "https://github.com/Lix1993/lix1993.github.io" }
     ],
     displayAllHeaders: true,
     lastUpdated: 'Last Updated'
